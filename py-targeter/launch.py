78fcb93bbbc7143382ae4d24a73d48cb8b8182cf
ericlecoutre <eric.lecoutre@welovedatascience.com>
2023-07-08 14:52:07 +0000

    deleted unnecessary files

--- py-targeter/launch.py
+++
@@ -1,5 +0,0 @@
-import papermill
-exec(open('./targeter.py').read())
-cmd='quarto render targeter-report.qmd --output generated_report  -P tmpdir:"C:\Users\natha\AppData\Local\Temp\targeter_ogarwusp" --to html'
-p = subprocess.Popen(cmd, cwd=tmpdir, shell=False)
-p.wait()
